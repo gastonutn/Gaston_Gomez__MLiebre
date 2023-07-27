f57910b610bd4a4d3a7ef4f0c024ab1e5be1317c
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-07-26 21:55:18 -0300

    formularios

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ app.use(express.static("public"))
 
 app.listen(PORT, () => console.log(`servidor corriendo en http://localhost:`+PORT))
 
-app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html"))) 
+
+app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")))
 
